e82db86e1d7a18254df285a242c69fd82035cbd4
Pedro <preis@student.bridgew.edu>
2020-01-23 11:35:11 -0500

    commit w comment

--- src/Bro.java
+++ src/Bro.java
@@ -1,5 +1,6 @@
 public class Bro {
     public static void main(String[] args){
+        // instead of #, example comment
         System.out.println("Hello Comp152");
         System.out.println("Lets make it work");
     }
